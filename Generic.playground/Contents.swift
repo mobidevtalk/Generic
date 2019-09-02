f37c9f7ea8a3adf354e476e662da65e4388b8afa
Shad Mazumder <shadmazumder@gmail.com>
2019-09-02 06:40:48 +0600

    Adding Int.

--- Generic.playground/Contents.swift
+++ Generic.playground/Contents.swift
@@ -8,6 +8,12 @@ extension String{
     }
 }
 
+extension Int{
+    var isPalindrome: Bool{
+        return String(self).isPalindrome
+    }
+}
+
 
 /*:
  Test Suite
@@ -15,9 +21,8 @@ extension String{
 import XCTest
 
 class TestSuite: XCTestCase{
-    /*:
-     String type
-     */
+    
+    // MARK: - String Type
     func test_nonPalindrome_string_false() {
         let input = "nonPalindrome"
         XCTAssertFalse(input.isPalindrome, "\(input) is not be a Palindrome")
@@ -33,12 +38,12 @@ class TestSuite: XCTestCase{
         XCTAssertFalse(input.isPalindrome, "Should have return flase for: \n \(input)")
     }
     
-    func test_palindrom_text_true() {
+    func test_palindrome_text_true() {
         let input = "wow wow"
         XCTAssertTrue(input.isPalindrome, "Should have return true for: \(input)")
     }
     
-    func test_nonPalindrom_textWithSpace_false() {
+    func test_nonPalindrome_textWithSpace_false() {
         let input = "wow wow "
         XCTAssertFalse(input.isPalindrome, "Should have return true for: \(input)")
     }
@@ -48,10 +53,38 @@ class TestSuite: XCTestCase{
         XCTAssertFalse(input.isPalindrome, "Should return false as ther is a capital W on the\n \(input)")
     }
     
-    func test_palindrom_mixCase_true() {
+    func test_palindrome_mixCase_true() {
         let input = "Wow woW"
         XCTAssertTrue(input.isPalindrome, "Even case is mixed but this is a palindrome text:\n \(input)")
     }
+    
+    // MARK: - Int
+    func test_nonPalindrome_Int_false(){
+        let input = 100
+        XCTAssertFalse(input.isPalindrome, "\(input) is not a palindrome Int")
+    }
+    
+    func test_palindrome_Int_true() {
+        let input = 121
+        XCTAssertTrue(input.isPalindrome, "\(input) is a palindrome Int")
+    }
+    
+    func test_longPalindrom_Int_true() {
+        let input = 0000000000
+        XCTAssertTrue(input.isPalindrome, "Long sequence on Zero should be a palindrome")
+    }
+    
+    func test_nonPalindrome_negativeInt_false() {
+        let input = -111
+        XCTAssertFalse(input.isPalindrome, "\(input) should not be a palindrome")
+    }
+    
+    func test_nonPalindrome_signedInt_fals() {
+        let input = +111
+        XCTAssertFalse(input.isPalindrome, "Signed input \(input) should not be a palindrome")
+    }
+    
+    
 }
 
 class TestObserver: NSObject, XCTestObservation {
